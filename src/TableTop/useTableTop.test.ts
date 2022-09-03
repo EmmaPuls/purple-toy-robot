8dd5277bb6581d58f117150c35c3a06a0ba18867
Emma Puls <got2love@gmail.com>
2022-09-04 01:39:55 +0800

    Fixed failing tests

--- src/TableTop/useTableTop.test.ts
+++ src/TableTop/useTableTop.test.ts
@@ -9,15 +9,15 @@ import {
 describe("useTableTop", () => {
   describe("_isEvenRow", () => {
     describe("given even row", () => {
-      const itemsInEvenRow = [GRID_SIZE * 0, GRID_SIZE * 2];
-      test.each(itemsInEvenRow)("returns true", (index) => {
+      const indicesOfEvenRows = [0, 2, 4];
+      test.each(indicesOfEvenRows)("returns true", (index) => {
         expect(_isEvenRow(index)).toBe(true);
       });
     });
 
     describe("given odd row", () => {
-      const itemsInOddRow = [GRID_SIZE * 1, GRID_SIZE * 3];
-      test.each(itemsInOddRow)("returns false", (index) => {
+      const indicesOfOddRows = [1, 3, 5];
+      test.each(indicesOfOddRows)("returns false", (index) => {
         expect(_isEvenRow(index)).toBe(false);
       });
     });
@@ -25,15 +25,15 @@ describe("useTableTop", () => {
 
   describe("_isEvenColumn", () => {
     describe("given even column", () => {
-      const itemsInEvenColumn = [0, 2, 4];
-      test.each(itemsInEvenColumn)("returns true", (index) => {
+      const indicesOfEvenCols = [GRID_SIZE * 0, GRID_SIZE * 2];
+      test.each(indicesOfEvenCols)("returns true", (index) => {
         expect(_isEvenColumn(index)).toBe(true);
       });
     });
 
     describe("given odd column", () => {
-      const itemsInOddColumn = [1, 3, 5];
-      test.each(itemsInOddColumn)("returns false", (index) => {
+      const indicesOfOddCols = [GRID_SIZE * 1, GRID_SIZE * 3];
+      test.each(indicesOfOddCols)("returns false", (index) => {
         expect(_isEvenColumn(index)).toBe(false);
       });
     });
