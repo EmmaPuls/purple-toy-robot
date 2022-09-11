b7d7c4991d10fd67e885859c0c726943832e2009
Emma Puls <got2love@gmail.com>
2022-09-11 18:41:00 +0800

    Fixed tests

--- src/features/Robot/useRobot.test.ts
+++ src/features/Robot/useRobot.test.ts
@@ -1,13 +1,20 @@
 import { RobotDirection } from "features/types";
 import { directionMap } from "./useRobot";
 
-describe('useRobot', () => {
-    describe('directionMap', () => {
-        it('should have the correct values', () => {
-            expect(directionMap[RobotDirection.NORTH]).toEqual('rotate(0deg)');
-            expect(directionMap[RobotDirection.SOUTH]).toEqual('rotate(180deg)');
-            expect(directionMap[RobotDirection.EAST]).toEqual('rotate(90deg)');
-            expect(directionMap[RobotDirection.WEST]).toEqual('rotate(270deg)');
-        });
+describe("useRobot", () => {
+  describe("directionMap", () => {
+    it("should map enums to correct values", () => {
+      expect(directionMap[RobotDirection.NORTH]).toEqual("rotate(270deg)",);
+      expect(directionMap[RobotDirection.SOUTH]).toEqual("rotate(90deg)");
+      expect(directionMap[RobotDirection.EAST]).toEqual("rotate(0deg)");
+      expect(directionMap[RobotDirection.WEST]).toEqual("rotate(180deg)");
     });
+
+    it("should map strings to correct values", () => {
+      expect(directionMap["NORTH"]).toEqual("rotate(270deg)",);
+      expect(directionMap["SOUTH"]).toEqual("rotate(90deg)");
+      expect(directionMap["EAST"]).toEqual("rotate(0deg)");
+      expect(directionMap["WEST"]).toEqual("rotate(180deg)");
+    });
+  });
 });
